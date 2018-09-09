7cfda5efb09c892ea44d5d94a05d9309c3edf0c9
Eliah Kagan <degeneracypressure@gmail.com>
2018-09-09 14:27:54 -0400

    another test
    Co-authored-by: David Vassallo <vassallo.davidm@gmail.com>

--- search.cpp
+++ search.cpp
@@ -13,6 +13,8 @@ void vector_print(const std::vector<T> &vec)
 int main()
 {
     std::vector test {1,2,3};
+    std::vector test2 {"hello","my","friend"};
     vector_print (test);
+    vector_print (test2);
     std::cout << "...your feelings. You know it to be true!\n"; // searching it is!
 }
