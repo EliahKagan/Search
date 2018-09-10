03da9c76b1eec79b1a792379d776497ac2b1ea0c
Eliah Kagan <degeneracypressure@gmail.com>
2018-09-09 22:18:21 -0400

    (Eliah) made the Noisy class and the Pool test
    Co-authored-by: David Vassallo <vassallo.davidm@gmail.com>

--- Pool-test.cpp
+++ Pool-test.cpp
@@ -1,5 +1,6 @@
 #include "Pool-test.h"
 #include <iostream>
+#include "Pool.h"
 
 namespace {
     class Noisy;
@@ -8,11 +9,41 @@ namespace {
 
     class Noisy {
     public:
-        Noisy() { std::cout << "Constructed " << *this << '\n'; }
+        Noisy() { std::cout << "Default constructed " << *this << '\n'; }
+
+        Noisy(const Noisy&) { std::cout << "Copy constructed " << *this << '\n'; }
+
+        Noisy(Noisy&&) { std::cout << "Move constructed " << *this << '\n'; }
+
+        Noisy& operator=(const Noisy&)
+        {
+            std::cout << "Copy assigned to " << *this << '\n';
+            return *this;
+        }
+
+        Noisy& operator=(Noisy&&)
+        {
+            std::cout << "Move assigned to " << *this << '\n';
+            return *this;
+        }
+
+        Noisy() { std::cout << "Destroying " << *this << '\n'; }
     };
+
+    std::ostream& operator<<(std::ostream& out, const Noisy& noisy)
+    {
+        return out << static_cast<const void*>(&noisy);
+    }
 }
 
 void test_pool()
 {
+    std::cout << '\n';
 
+    Pool<Noisy> pool;
+    for (auto i = 5; i != 0; --i)
+        std::cout << '[' << *pool.newmem() << "]\n";
+
+    std::cout << "\nPress enter now... ";
+    std::cin.get();
 }
