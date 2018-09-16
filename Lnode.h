37eef2cb5295675375021d447b19e2d8256e31c5
Eliah Kagan <degeneracypressure@gmail.com>
2018-09-15 22:17:14 -0400

    (David) implemented make_list, building back-to-front
    Co-authored-by: David Vassallo <vassallo.davidm@gmail.com>

--- Lnode.h
+++ Lnode.h
@@ -14,11 +14,13 @@ struct Lnode
 template <typename T>
 Lnode<T>* make_list(const std::vector<T>& v, Pool<Lnode<T>>& p)
 {
-
     Lnode* head = nullptr;
-    for (auto i = v.rbegin(); i != v.rend(); ++i)
+    for (auto it = v.rbegin(); it != v.rend(); ++it)
     {
-
+        Lnode* temp = p.newmem();
+        temp->element = *it;
+        temp->next = head;
+        head = temp;
     }
 }
 
