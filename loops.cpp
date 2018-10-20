6555ab5595f14c24500b9bf5c27736c52181809b
Eliah Kagan <degeneracypressure@gmail.com>
2018-10-20 16:18:53 -0400

    a couple test vectors and skeleton for one implementation
    Co-authored-by: David Vassallo <vassallo.davidm@gmail.com>

--- loops.cpp
+++ loops.cpp
@@ -1,6 +1,21 @@
 #include "loops.h"
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+using namespace std::literals;
+
+namespace {
+    template <typename T>
+    T range_for(vector<T> a)
+    {
+
+    }
+}
 
 void test_loops()
 {
-
+    vector a {10, 7, 3, 9, -4, 14, 27, 13, -27, 13, 17, 99, 103};
+    vector b {"foo"s, "bar"s, "baz"s, "quux"s, "foobar"s};
 }
