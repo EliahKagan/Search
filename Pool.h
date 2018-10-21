7c2c09383c1c105751bf34969aaded9ff7aa9497
Eliah Kagan <degeneracypressure@gmail.com>
2018-10-20 21:42:15 -0400

    (Eliah) renamed newmem as operator() and implemented newmem as a compatibility wrapper
    Co-authored-by: David Vassallo <vassallo.davidm@gmail.com>

--- Pool.h
+++ Pool.h
@@ -8,7 +8,10 @@ class Pool
 {
     public:
         template<typename... Args>
-        T* newmem(Args&&... args);
+        T* operator()(Args&&... args);
+
+        template<typename... Args>
+        T* newmem(Args&&... args); // for backward compatibility
 
     private:
         std::deque<T> q;
@@ -16,9 +19,16 @@ class Pool
 
 template <typename T>
 template <typename... Args>
-T* Pool<T>::newmem(Args&&... args)
+T* Pool<T>::operator()(Args&&... args)
 {
    return &(q.emplace_back(std::forward<Args>(args)...));
+}
+
+template <typename T>
+template <typename... Args>
+inline T* Pool<T>::newmem(Args&&... args)
+{
+   return (*this)(std::forward<Args>(args)...);
 }
 
 #endif // ! POOLCLASS
