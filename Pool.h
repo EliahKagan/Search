3f320a303fc022646a77201b0dc79b84e9fafd55
Eliah Kagan <degeneracypressure@gmail.com>
2018-10-20 21:36:07 -0400

    (Eliah) enhanced newmem implementation and examples of move semantics and perfect forwarding
    Co-authored-by: David Vassallo <vassallo.davidm@gmail.com>

--- Pool.h
+++ Pool.h
@@ -7,15 +7,50 @@ template <typename T>
 class Pool
 {
     public:
-        T* newmem();
+        template<typename... Args>
+        T* newmem(Args&&... args);
+
     private:
         std::deque<T> q;
 };
 
 template <typename T>
-T* Pool<T>::newmem()
+template <typename... Args>
+T* Pool<T>::newmem(Args&&... args)
 {
-   return &(q.emplace_back());
+   return &(q.emplace_back(std::forward<Args>(args)...));
 }
+
+class Foo { };
+
+void g(const Foo&);
+void g(Foo&&);
+
+void f(const Foo& x)
+{
+    // do stuff with x
+
+    g(x);
+}
+
+void f(Foo&& x)
+{
+    // do stuff with x
+
+    g(std::move(x));
+}
+
+template<typename T>
+void h(T&& x)
+{
+    // do stuff with x
+
+    g(std::forward<T>(x));
+}
+
+template<typename T>
+class Bar {
+    void otherfunc(T&& x);
+};
 
 #endif // ! POOLCLASS
