e19d9d6b8efce5770662c9e5d63223cf801e2e81
Eliah Kagan <degeneracypressure@gmail.com>
2018-10-20 23:36:32 -0400

    (David) wrote test of tree_size
    Co-authored-by: David Vassallo <vassallo.davidm@gmail.com>

--- tree-test.cpp
+++ tree-test.cpp
@@ -1,10 +1,14 @@
 #include "tree-test.h"
 
+#include <iostream>
 #include "Pool.h"
 #include "Tnode.h"
+
+using namespace std;
 
 void test_trees()
 {
     Pool<Tnode<int>> f;
     auto root = f(3,f(4),f(5));
+    cout << tree_size(root) << '\n';
 }
