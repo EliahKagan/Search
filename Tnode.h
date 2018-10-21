a3711fc0e7ce05691f728fbb0d62dbad0752bf21
Eliah Kagan <degeneracypressure@gmail.com>
2018-10-20 23:14:55 -0400

    (David) implemented both Tnode constructors properly
    Co-authored-by: David Vassallo <vassallo.davidm@gmail.com>

--- Tnode.h
+++ Tnode.h
@@ -2,6 +2,7 @@
 #define TNODESTRUCT
 
 #include "Pool.h"
+#include <utility>
 
 template <typename T>
 struct Tnode
@@ -10,13 +11,9 @@ struct Tnode
     Tnode* left;
     Tnode* right;
 
-    Tnode(T p, Tnode* l, Tnode* r)
-    {
-        element = p;
-        left = l;
-        right = r;
-    }
-};
+    Tnode(T p, Tnode* l, Tnode* r) : element{std::move(p)}, left{l}, right{r} {}
+    Tnode(T p) : element{std::move(p)}, left{}, right{} {}
 
+};
 
 #endif //!TNODESTRUCT
