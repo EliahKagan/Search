72da0fd348a2cfd8fc02083658af66aabc5cf365
Eliah Kagan <degeneracypressure@gmail.com>
2018-10-20 23:45:36 -0400

    tested tree_size
    Co-authored-by: David Vassallo <vassallo.davidm@gmail.com>

--- tree-test.cpp
+++ tree-test.cpp
@@ -8,7 +8,9 @@ using namespace std;
 
 void test_trees()
 {
+    constexpr const Tnode<int>* empty {};
     Pool<Tnode<int>> f;
+
     auto root = f(3,f(4),f(5));
-    cout << tree_size(root) << '\n';
+    cout << tree_size(empty) << ' ' << tree_size(root) << '\n';
 }
