fb9ae9cf3a68eef57b8af9c2e46c52c2af972226
Eliah Kagan <degeneracypressure@gmail.com>
2018-10-27 13:44:53 -0400

    test to traverse a more complex tree

--- tree-test.cpp
+++ tree-test.cpp
@@ -12,9 +12,14 @@ void test_trees()
     constexpr const Tnode<int>* empty {};
     Pool<Tnode<int>> f;
 
-    auto root = f(3,f(4),f(5));
+    auto root = f(3, f(4), (5));
     cout << tree_size(empty) << ' ' << tree_size(root) << '\n';
 
     vector<int> v = tree_to_vector(root);
     print(v);
+
+    auto root2 = f(9, root,
+                      f(2, nullptr,
+                           f(6)));
+    print(tree_to_vector(root2));
 }
