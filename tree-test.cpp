e249f988467735c2ab4dad88fd4cf290d8fdcc94
Eliah Kagan <degeneracypressure@gmail.com>
2018-10-27 14:06:12 -0400

    fixed silly typo that sent us down a rabbit hole
    Co-authored-by: David Vassallo <vassallo.davidm@gmail.com>

--- tree-test.cpp
+++ tree-test.cpp
@@ -12,7 +12,7 @@ void test_trees()
     constexpr const Tnode<int>* empty {};
     Pool<Tnode<int>> f;
 
-    auto root = f(3, f(4), (5));
+    auto root = f(3, f(4), f(5));
     cout << tree_size(empty) << ' ' << tree_size(root) << '\n';
 
     vector<int> v = tree_to_vector(root);
